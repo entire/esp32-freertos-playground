987e068c9134410bba235240102a7aa0f93d826f
Kosuke Hata <hata.kosuke@gmail.com>
2022-10-31 12:41:02 -0700

    working on hw interrupt

--- esp32s2/hw-interrupt/hw-interrupt.ino
+++ esp32s2/hw-interrupt/hw-interrupt.ino
@@ -5,8 +5,8 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-static const int16_t timer_divider = 8; // clock ticks at 10mhz
-static const int64_t timer_max_count = 1000000;
+static const int16_t timer_divider = 80; // clock divider, clock ticks at 1mhz
+static const int64_t timer_max_count = 1000000; // 1 Mhz
 static const TickType_t task_delay = 2000 / portTICK_PERIOD_MS; // 2 secs
 
 // Settings
@@ -17,13 +17,57 @@ static const int led_pin = LED_BUILTIN;
 
 // Globals
 static hw_timer_t *timer = NULL;
+static volatile int isr_counter;
+static portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED; // spinlock is ESP32 specific 
+
+typedef struct {
+    uint8_t * const buffer;
+    int head;
+    int tail;
+    const int maxlen;
+} adc_sample_circ_bbuf_t;
+
+int circ_bbuf_push(adc_sample_circ_bbuf_t *c, uint8_t data)
+{
+    int next;
+
+    next = c->head + 1;  // next is where head will point to after this write.
+    if (next >= c->maxlen)
+        next = 0;
+
+    if (next == c->tail)  // if the head + 1 == tail, circular buffer is full
+        return -1;
+
+    c->buffer[c->head] = data;  // Load data and then move
+    c->head = next;             // head to next data offset.
+    return 0;  // return success to indicate successful push.
+}
+
+int circ_bbuf_pop(circ_bbuf_t *c, uint8_t *data)
+{
+    int next;
+
+    if (c->head == c->tail)  // if the head == tail, we don't have any data
+        return -1;
+
+    next = c->tail + 1;  // next is where tail will point to after this read.
+    if(next >= c->maxlen)
+        next = 0;
+
+    *data = c->buffer[c->tail];  // Read data and then move
+    c->tail = next;              // tail to next offset.
+    return 0;  // return success to indicate successful push.
+}
 
 //*****************************************************************************
 // ISR
 
 // this function executes when timer reaches max (and resets)
 void IRAM_ATTR onTimer() {
-  
+
+  // Toggle LED
+  int pin_state = digitalRead(led_pin);
+  digitalWrite(led_pin, !pin_state);
 }
 
 //*****************************************************************************
@@ -31,26 +75,26 @@ void IRAM_ATTR onTimer() {
 
 void readSerial(void *parameters) {
 
-  char c;
-  char buf[buf_len];
+  // char c;
+  // char buf[buf_len];
 
   
   // configure pins
-  pinMode(led_pin, OUTPUT);
+  // pinMode(led_pin, OUTPUT);
 
-  memset(buf, 0, buf_len);
+  // memset(buf, 0, buf_len);
 
   // loop forever
-  while (1) {
-    if (Serial.available() > 0) {
-      c = Serial.read();
+  // while (1) {
+  //   if (Serial.available() > 0) {
+  //     c = Serial.read();
 
-    if (c == '\n') {
+  //   if (c == '\n') {
       
-    } else {
+  //   } else {
 
-    }
-  }
+  //   }
+  // }
 }
 
 
@@ -63,6 +107,19 @@ void setup() {
   while (!Serial);
   Serial.println("Starting HW Interrupt Demo"); 
 
+  // create and start timer (num, divider, countUp)
+  timer = timerBegin(0, timer_divider, true);
+
+  // provide ISR to timer (timer, function, edge)
+  timerAttachInterrupt(timer, &onTimer, true);
+
+  // at what count should ISR trigger (timer, count, autoreload)
+  timerAlarmWrite(timer, timer_max_count, true);
+
+  // allow ISR to trigger
+  timerAlarmEnable(timer);
+
+
   xTaskCreatePinnedToCore(
     readSerial,     // Function to be called
     "Read Serial",  // Name of task
@@ -73,10 +130,8 @@ void setup() {
     app_cpu         // Run on one core for demo purposes (ESP32 only)
   );
 
-  // delete "setup and loop" task
-  vTaskDelete(NULL);
 }
 
 void loop() {
-
+  // do nothing
 }
